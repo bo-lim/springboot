627c5d5e1cd35df877e932b16197a9aee5e38627
ant67410 <ant67410@gmail.com>
2021-02-03 10:05:48 +0900

    두번째 시도

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,7 +7,7 @@ source ${ABSDIR}/profile.sh
 REPOSITORY=/home/ec2-user/app/step3
 PROJECT_NAME=springbootAWS
 
-echo "> Buildl 파일복사"
+echo "> Build 파일복사"
 echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
@@ -16,7 +16,7 @@ echo "> 새 애플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name : $JAR_NAME"
-echo "> $JAR_Name 에 실행권한 추가"
+echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 IDLE_PROFILE=$(find_idle_profile)
